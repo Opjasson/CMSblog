1be2431e15bc5cbb10d10825c91cda5e29aa6967
Opjasson <azmitheusos333@gmail.com>
2024-11-03 17:08:23 +0700

    add dashboard controller,route, and layout

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LoginController ;
+use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\HomeController as SiteHomeController;
 
 
 
 // ->name berfungsi untuk memberikan nama pada route dan supaya bisa diigunakan didalam aplikasi 
-Route::get('/',[HomeController::class, 'index'])->name('site.home');
+Route::get('/',[SiteHomeController::class, 'index'])->name('site.home');
 
-Route::get('/login',[LoginController::class, 'index'])->name('site.login');
+Route::get('/login',[LoginController::class, 'index'])->name('auth.login');
+
+Route::get('/dashboard',[HomeController::class, 'index'])->name('dashboard.home');
