d42df2885842a0569feb9f9108b685107a07a419
Opjasson <azmitheusos333@gmail.com>
2024-11-07 07:09:51 +0700

    add login page

--- resources/views/layouts/auth/auth-layout.blade.php
+++ resources/views/layouts/auth/auth-layout.blade.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $app['name'] }} | @yield('title')</title>
-    @vite(['resources/sass/app.scss'])
+    @vite('resources/sass/app.scss')
 </head>
 
 <body class="bg-primary">
@@ -24,6 +24,7 @@
           </main>
        </div>
        {{-- footer --}}
+       
        @include('layouts.auth._footer')
       </div>
  
