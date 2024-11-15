fc6a8ffbe93f917491bab41ae214aa51100dba2f
Opjasson <azmitheusos333@gmail.com>
2024-11-15 14:26:18 +0700

    add tag value view

--- app/Http/Controllers/Dashboard/TagController.php
+++ app/Http/Controllers/Dashboard/TagController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tags;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
     //
     public function index() {
-        return view('dashboard.tag.index');
+        
+        return view('dashboard.tag.index', [
+            'tags' => Tags::take(10)->get()
+        ]);
     }
 }
--- resources/views/dashboard/tag/index.blade.php
+++ resources/views/dashboard/tag/index.blade.php
@@ -7,5 +7,27 @@
 @endsection
 
 @section('content')
-    Tag list
+<div class="card">
+    <div class="card-body">
+       <table class="table table-hover table-bordered table-hover">
+          <thead>
+             <tr>
+                <th>Tag</th>
+                <th>Action</th>
+             </tr>
+          </thead>
+          <tbody>
+             @foreach ($tags as $tag)
+                <tr>
+                   <td>{{ $tag->title }}</td>
+                   <td>
+                      <button type="button" class="btn btn-primary">Edit</button>
+                      <button type="button" class="btn btn-danger">Delete</button>
+                   </td>
+                </tr>
+             @endforeach
+          </tbody>
+       </table>
+    </div>
+ </div>
 @endsection
